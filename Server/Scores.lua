5026cd5d9f1fb4c426a5df833e5b7022e3c27397
ota <otavio.cts@gmail.com>
2026-03-15 21:54:24 -0300

    Refactor scoreboard file

---
+++ Server/Scores.lua
@@ -0,0 +1,50 @@
+PlayerScores = {}
+PlayerCombos = {}
+PlayerComboTimers = {}
+AAA = 0
+
+function BroadcastKill(instigator, victimCharacter)
+  local killerPlayer = instigator and instigator:IsValid() and instigator or nil
+  local killerName = killerPlayer and killerPlayer:GetName() or "Unknown"
+  -- concat killername with random string
+  killerName = killerName .. " " .. AAA
+  AAA=AAA+1
+  local victimPlayer = victimCharacter:GetPlayer()
+  local victimName = victimPlayer and victimPlayer:IsValid() and victimPlayer:GetName() or "Bot"
+  Events.BroadcastRemote("KillFeed", killerName, victimName, Config.KillFeedDuration)
+
+  if killerPlayer then
+    local killerId = killerPlayer:GetAccountID()
+
+    if not PlayerScores[killerId] then
+      PlayerScores[killerId] = { id = killerId, name = killerName, icon = killerPlayer:GetAccountIconURL(), kills = 0 }
+    end
+    PlayerScores[killerId].kills = PlayerScores[killerId].kills + 1
+
+    PlayerCombos[killerId] = (PlayerCombos[killerId] or 0) + 1
+    local combo = PlayerCombos[killerId]
+    Events.CallRemote("UpdateCombo", killerPlayer, combo)
+
+    if PlayerComboTimers[killerId] then
+      Timer.ClearTimeout(PlayerComboTimers[killerId])
+    end
+    PlayerComboTimers[killerId] = Timer.SetTimeout(function()
+      PlayerCombos[killerId] = 0
+      if killerPlayer:IsValid() then
+        Events.CallRemote("UpdateCombo", killerPlayer, 0)
+      end
+      PlayerComboTimers[killerId] = nil
+    end, Config.ComboDuration * 1000)
+
+    BroadcastScoreboard()
+  end
+end
+
+function BroadcastScoreboard()
+  local list = {}
+  for _, data in pairs(PlayerScores) do
+    table.insert(list, data)
+  end
+  table.sort(list, function(a, b) return a.kills > b.kills end)
+  Events.BroadcastRemote("UpdateScoreboard", list, Config.LeaderboardMaxTop)
+end

54e55a8bf481df738630867f192c5a7942aa6b8e
ota <otavio.cts@gmail.com>
2026-03-18 01:22:08 -0300

    Increase bonker size slightly

--- Server/Bonker.lua
+++ Server/Bonker.lua
@@ -3,7 +3,7 @@ Bonker = Melee.Inherit("Bonker")
 function Bonker:Constructor(location, rotation)
 	self.Super:Constructor(location or Vector(), rotation or Rotator(), "nanos-world::SM_BaseballBat_01", CollisionType.Normal, true, HandlingMode.SingleHandedMelee, "")
 
-	self:SetScale(1.5)
+	self:SetScale(1.6)
     self:AddAnimationCharacterUse("nanos-world::AM_Mannequin_Melee_Slash_Attack")
     self:SetDamageSettings(0.3, 0.5)
     self:SetCooldown(1.0)
